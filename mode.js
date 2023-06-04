bd04f71c96466a3d08a6130c2181ce072bd8e47d
chuchei <chuchei5243@gmail.com>
2023-06-04 01:00:08 -0500

    solucion de cargado

--- mode.js
+++ mode.js
@@ -2,7 +2,6 @@ const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 
-
 const themeToggler=document.querySelector(".theme-toggler");
 
 //mostrar menu
@@ -14,7 +13,6 @@ menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none';
   });
 
-
 //cambiar tema
 themeToggler.addEventListener('click',()=> {
     document.body.classList.toggle('dark-theme-variables');
@@ -22,7 +20,6 @@ themeToggler.addEventListener('click',()=> {
 
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
-
 
 
 });
